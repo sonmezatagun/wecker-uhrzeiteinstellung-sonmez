ab5e42e342c0aaa4d20390e67b9613b63729a791
sonmezatagun <ataguen.soenmez@stud.th-deg.de>
2025-11-07 17:33:13 +0100

    Update TimeSetLogic.java

--- src/main/java/logic/TimeSetLogic.java
+++ src/main/java/logic/TimeSetLogic.java
@@ -1,15 +1,5 @@
 package logic;
 
 public class TimeSetLogic {
-
-    private static TimeSetLogic instance;
-
-    private TimeSetLogic() { }
-
-    public static synchronized TimeSetLogic getInstance() {
-        if (instance == null) {
-            instance = new TimeSetLogic();
-        }
-        return instance;
-    }
+    // TODO: will be implemented later
 }
